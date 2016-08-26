3d12a10e1dd04a5b95b1112985b455b5e9800d58
somesayss <413461856@qq.com>
2016-08-26 15:58:36 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 // 依赖
 const gulp = require('gulp');
-const browserSync = require('browser-sync');
 const babel = require('gulp-babel');
+const uglify= require('gulp-uglify');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync');
 
 // 任务
-gulp.task('default', ['babel']);
+gulp.task('default', ['zip']);
 gulp.task('server', ['brow', 'watch']);
 
 // 静态服务
@@ -26,7 +28,26 @@ gulp.task('babel', () => {
 		.pipe(gulp.dest('dist'));
 });
 
+// 压缩
+gulp.task('zip', ['babel'], () => {
+    return gulp.src(['dist/**/*.js'])
+        .pipe( uglify() )
+        .pipe( rename({suffix: '.min'}) )
+        .pipe(gulp.dest('dist'));
+});
+
 // 自动编译
 gulp.task('watch', () => {
-	gulp.watch(['src/**/*.js'], ['babel']);
+    const matchRex = /src((?:\/.*\/)?).*\..*/;
+    gulp.watch(['src/**/*.js'])
+        .on('change', (e) => {
+            var match = e.path.replace(/\\/g, '/').match( matchRex ),
+                file = match[0];
+            // console.log('File ' + file + ' was ' + e.type);
+            gulp.src( file )
+                .pipe( babel( { presets: ['es2015', 'stage-0'] } ).on('error', (e) => {
+                    console.error('error', e.message);
+                }) )
+                .pipe(gulp.dest('dist' + match[1]));
+        });
 });
